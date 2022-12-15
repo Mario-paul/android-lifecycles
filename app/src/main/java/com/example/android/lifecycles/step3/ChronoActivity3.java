130afc3dd317306e7bb310edf1cbd4f2bcb9c9ca
Mario-paul <15961362+Mario-paul@users.noreply.github.com>
2022-12-14 22:05:36 -0500

    step 3 - wrap data using LiveData

--- app/src/main/java/com/example/android/lifecycles/step3/ChronoActivity3.java
+++ app/src/main/java/com/example/android/lifecycles/step3/ChronoActivity3.java
@@ -18,9 +18,12 @@ package com.example.android.lifecycles.step3;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 import android.widget.TextView;
 
@@ -53,6 +56,6 @@ public class ChronoActivity3 extends AppCompatActivity {
             }
         };
 
-        //TODO: observe the ViewModel's elapsed time
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver); // step 3 - observe the ViewModel's elapsed time
     }
 }
